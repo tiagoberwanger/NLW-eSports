d17ee257d4ff296bc21c630f58530716c4deb46f
Tiago Berwanger <tiago.berwanger@mercos.com>
2022-09-15 19:43:35 -0300

    faz chamada pro backend para retornar games

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,76 +1,39 @@
-import { MagnifyingGlassPlus } from 'phosphor-react'
+import { useEffect, useState } from 'react'
+
+import GameBanner from './components/GameBanner'
+import CreateAdBanner from './components/CreateAdBanner'
+import Header from './components/Header'
 
 import './styles/main.css'
 
-import logoImg from './assets/logo-nlw-esports.png'
-
 function App() {
-  return (
-    <div className="max-w-[1344px] mx-auto flex items-center flex-col my-20">
-      <img src={logoImg} alt="logo" />
-      <h1 className="text-6xl text-white font-black mt-20">
-        Seu <span className='text-transparent bg-clip-text bg-nlw-gradient'>duo</span> está aqui.
-      </h1>
+    const [games, setGames] = useState([])
 
-      <div className='grid grid-cols-6 gap-6 mt-16'>
-        <a href='' className='relative rounded-lg overflow-hidden'>
-          <img src='/game-1.png' alt='game' />
-          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-            <strong className='font-bold text-white block'>Jogo 1</strong>
-            <span className='text-zinc-300 text-sm block'> 4 anuncios</span>
-          </div>
-        </a>
-        <a href='' className='relative rounded-lg overflow-hidden'>
-          <img src='/game-2.png' alt='game' />
-          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-            <strong className='font-bold text-white block'>Jogo 2</strong>
-            <span className='text-zinc-300 text-sm block'> 4 anuncios</span>
-          </div>
-        </a>
-        <a href='' className='relative rounded-lg overflow-hidden'>
-          <img src='/game-3.png' alt='game' />
-          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-            <strong className='font-bold text-white block'>jogo 3</strong>
-            <span className='text-zinc-300 text-sm block'> 4 anuncios</span>
-          </div>
-        </a>
-        <a href='' className='relative rounded-lg overflow-hidden'>
-          <img src='/game-4.png' alt='game' />
-          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-            <strong className='font-bold text-white block'>Jogo 4</strong>
-            <span className='text-zinc-300 text-sm block'> 4 anuncios</span>
-          </div>
-        </a>
-        <a href='' className='relative rounded-lg overflow-hidden'>
-          <img src='/game-5.png' alt='game' />
-          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-            <strong className='font-bold text-white block'>Jogo 5</strong>
-            <span className='text-zinc-300 text-sm block'> 4 anuncios</span>
-          </div>
-        </a>
-        <a href='' className='relative rounded-lg overflow-hidden'>
-          <img src='/game-6.png' alt='game' />
-          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-            <strong className='font-bold text-white block'>jogo 6</strong>
-            <span className='text-zinc-300 text-sm block'> 4 anuncios</span>
-          </div>
-        </a>
-      </div>
-      <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg mt-8 overflow-hidden">
-        <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center">
-          <div>
-            <strong className="text-2xl text-white font-black block">Não encontrou seu duo?</strong>
-            <span className="text-zinc-400 block">Publique um anúncio para encontrar novos players!</span>
-          </div>
+    useEffect(() => {
+        fetch('http://localhost:3333/games')
+            .then(response => response.json())
+            .then(data => setGames(data))
+    }, [])
 
-          <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
-            <MagnifyingGlassPlus size={24} />
-            Publicar anúncio
-          </button>
+    return (
+        <div className="max-w-[1344px] mx-auto flex items-center flex-col my-20">
+            <Header />
+
+            <div className='grid grid-cols-6 gap-6 mt-16'>
+                {games.map((game) =>
+                    <GameBanner
+                        key={game['id']}
+                        bannerUrl={game['bannerUrl']}
+                        title={game['title']}
+                        adsCount={game['_count']['ads'] || 0}
+                    />
+                )}
+            </div>
+            <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg mt-8 overflow-hidden">
+                <CreateAdBanner />
+            </div>
         </div>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default App
