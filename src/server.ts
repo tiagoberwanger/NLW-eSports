5968f999d26dcb43aa0257cd532edc1f9950ed21
Tiago Berwanger <tiago.berwanger@mercos.com>
2022-09-15 12:11:48 -0300

    add métodos HTTP para games e ads

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,23 @@
-import express from 'express'
+import express from 'express';
 
-const app = express()
+const app = express();
 
-app.get('/ads', (request, response) => {})
+app.get('/games', (request, response) => {
+    return response.json([])
+})
 
-app.listen(3333)
+app.post('/ads', (request, response) => {
+    return response.status(201).json([])
+})
+
+app.get('/games/:id/ads', (request, response) => {
+    const gameId = request.params.id
+    return response.json(gameId)
+})
+
+app.get('/ads/:id/discord', (request, response) => {
+    const adId = request.params.id
+    return response.json(adId)
+})
+
+app.listen(3333);
