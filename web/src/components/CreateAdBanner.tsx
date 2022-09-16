193fee99d93b982e61195548bd201811e1fb7b6b
Tiago Berwanger <tiago.berwanger@mercos.com>
2022-09-15 22:12:08 -0300

    cria modal para add ads

--- web/src/components/CreateAdBanner.tsx
+++ web/src/components/CreateAdBanner.tsx
@@ -1,6 +1,7 @@
 import { MagnifyingGlassPlus } from 'phosphor-react';
+import * as Dialog from '@radix-ui/react-dialog'
 
-function CreateAdBanner() {
+export function CreateAdBanner() {
     return (
         <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center">
             <div>
@@ -8,12 +9,10 @@ function CreateAdBanner() {
                 <span className="text-zinc-400 block">Publique um anúncio para encontrar novos players!</span>
             </div>
 
-            <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
+            <Dialog.Trigger className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
                 <MagnifyingGlassPlus size={24} />
                 Publicar anúncio
-            </button>
+            </Dialog.Trigger>
         </div>
     );
 }
-
-export default CreateAdBanner;
